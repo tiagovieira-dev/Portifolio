d8c1c88a7a3b80ba154e64935723f4c0ecee72ef
Tiago Vieira <tiagoseinfert@gmail.com>
2026-03-19 00:06:53 -0300

    Modificações Diversas
    Modificações diversas feitas em todas as páginas do site e estilização também.

---
+++ components/background.js
@@ -0,0 +1,29 @@
+new FinisherHeader({
+        count: 40,
+        size: {
+          min: 8,
+          max: 8,
+          pulse: 0,
+        },
+        speed: {
+          x: {
+            min: 0,
+            max: 0.3,
+          },
+          y: {
+            min: 0,
+            max: 0.3,
+          },
+        },
+        colors: {
+          background: "#000512",
+          particles: ["#8184e2"],
+        },
+        blending: "overlay",
+        opacity: {
+          center: 0.4,
+          edge: 0,
+        },
+        skew: 0,
+        shapes: ["c"],
+      });
